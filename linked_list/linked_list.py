be6eb41b122c0202bb5c3f2674f467e408b51f0b
Jels <jelscode@gmail.com>
2021-02-20 20:20:15 -0800

    more linkedlist methods, including iter, and tests

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -19,7 +19,20 @@ class LinkedList:
             node = node.next
 
         nodeList.append('None')
-        return ' -> '.join(nodeList)
+        return ' -> '.join(str(v) for v in nodeList)
+
+    def __iter__(self):
+        if self.head is None:
+            return
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    # for testing purposes
+    def insert_from_list(self, list):
+        for x in list:
+            self.append(x)
 
     def append(self, data):
         new_node = Node(data)
@@ -53,7 +66,7 @@ class LinkedList:
 
     def delete_node(self, key):
         if self.head is None:
-            return
+            raise TypeError("Linkedlist is empty")
 
         if self.head.data == key:
             self.head = self.head.next
@@ -65,4 +78,68 @@ class LinkedList:
                 curr_node.next = curr_node.next.next
                 return
             curr_node = curr_node.next
-        #  node not found
+        raise ValueError(f'Node {key} was not found')
+
+    def delete_node_at_pos(self, pos):
+        if pos < 0:
+            raise IndexError(f'Index {pos} cannot be negative')
+        if pos == 0:
+            self.head = self.head.next
+            return
+
+        prev_node = self.head
+
+        for i in range(1, pos):
+            prev_node = prev_node.next
+            if prev_node is None:
+                raise IndexError(f'Index {pos} is out of range')
+
+        if prev_node is None or prev_node.next is None:
+            raise IndexError(f'Index {pos} is out of range')
+
+        prev_node.next = prev_node.next.next
+
+    def len(self):
+        if self.head is None:
+            return 0
+        length = 0
+
+        for node in self:
+            length += 1
+            node = node.next
+
+        return length
+
+    def swap(self, key_1, key_2):
+
+        if self.head is None:
+            return
+
+        prev_1 = None
+        curr_1 = self.head
+        while curr_1.data is not key_1:
+            prev_1 = curr_1
+            curr_1 = curr_1.next
+            if curr_1 is None:
+                raise ValueError(f'{key_1} is not in the list')
+
+        prev_2 = None
+        curr_2 = self.head
+        while curr_2.data is not key_2:
+            prev_2 = curr_2
+            curr_2 = curr_2.next
+            if curr_2 is None:
+                raise ValueError(f'{key_2} is not in the list')
+
+        # then prev_1 is not head node
+        if prev_1 is not None:
+            prev_1.next = curr_2
+        else:
+            self.head = curr_2
+
+        if prev_2 is not None:
+            prev_2.next = curr_1
+        else:
+            self.head = curr_2
+
+        curr_1.next, curr_2.next = curr_2.next, curr_1.next
--- linked_list/test_linked_list.py
+++ linked_list/test_linked_list.py
@@ -4,6 +4,15 @@ from linked_list import LinkedList, Node
 
 class TestStack(unittest.TestCase):
 
+    def test_iter(self):
+        list = LinkedList()
+        list.insert_from_list(['A', 'A', 'A'])
+        count = 0
+        for node in list:
+            self.assertEqual(node.data, 'A')
+            count += 1
+        self.assertEqual(count, 3)
+
     def test_append(self):
         list = LinkedList()
         list.append('A')
@@ -14,38 +23,68 @@ class TestStack(unittest.TestCase):
     def test_prepend(self):
         list = LinkedList()
         list.append('B')
-        list.prepend('A')
 
+        list.prepend('A')
         self.assertEqual(repr(list), 'A -> B -> None')
 
     def test_insert_after_node(self):
         list = LinkedList()
-        list.append('A')
-        list.append('C')
-        list.insert_after_node(list.head, 'B')
+        list.insert_from_list(['A', 'C', 'D', 'E'])
 
-        self.assertEqual(repr(list), 'A -> B -> C -> None')
+        list.insert_after_node(list.head, 'B')
+        self.assertEqual(repr(list), 'A -> B -> C -> D -> E -> None')
 
     def test_delete_node(self):
         list = LinkedList()
-        list.append('A')
-        list.append('B')
+        list.insert_from_list(['A', 'B'])
 
-        list.delete_node('non-existent-node')
-        self.assertEqual(repr(list), 'A -> B -> None')
+        self.assertRaises(ValueError, list.delete_node, 'non-existent-node')
 
         list.delete_node('B')
-        self.assertEqual(repr(list), 'A -> None')   
+        self.assertEqual(repr(list), 'A -> None')
 
         list.append('B2')
         list.delete_node('A')
-        self.assertEqual(repr(list), 'B2 -> None')   
+        self.assertEqual(repr(list), 'B2 -> None')
 
         list.delete_node('B2')
-        self.assertEqual(repr(list), 'None')   
+        self.assertEqual(repr(list), 'None')
 
-        list.delete_node('non-existent-node')
-        self.assertIsNone(list.head)
+        self.assertRaises(TypeError, list.delete_node, 'non-existent-node')
+
+    def test_delete_node_at_pos(self):
+        list = LinkedList()
+        list.insert_from_list([0, 1, 2, 3])
+        list.delete_node_at_pos(2)
+
+        self.assertEqual(repr(list), '0 -> 1 -> 3 -> None')
+
+        self.assertRaises(IndexError, list.delete_node_at_pos, 3)
+        self.assertRaises(IndexError, list.delete_node_at_pos, -2)
+        self.assertRaises(IndexError, list.delete_node_at_pos, 10)
+
+    def test_len(self):
+        list = LinkedList()
+        list.insert_from_list([0, 1, 2, 3, 4])
+
+        self.assertEqual(list.len(), 5)
+
+    def test_len_zero(self):
+        list = LinkedList()
+        self.assertEqual(list.len(), 0)
+
+    def test_swap(self):
+        list = LinkedList()
+        list.insert_from_list(['A', 'B', 'C', 'D', 'E'])
+        list.swap('B', 'D')
+        self.assertEqual(repr(list), 'A -> D -> C -> B -> E -> None')
+
+    def test_reverse(self):
+        list = LinkedList()
+        list.insert_from_list(['A', 'B', 'C', 'D', 'E'])
+        list.reverse()
+        self.assertEqual(repr(list), 'E -> D -> C -> B -> A -> None')
+
 
 
 # runs directly without using unittest in command line
